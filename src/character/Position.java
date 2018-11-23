21a87e14ab94db2f86f43a8a6cc52b0ceacf5460
Pornapat Sudjaipraparat <pornapat04@gmail.com>
2018-11-23 18:42:18 +0700

    Edit Position class

--- src/character/Position.java
+++ src/character/Position.java
@@ -6,24 +6,8 @@ public abstract class Position {
 	protected double x;
 	protected double y;
 	
-	public Position() {
-		this.x = 0;
-		this.y = 0;
-	}
 	public Position(double x, double y) {
 		this.x = x;
-		this.y = y;
-	}
-	public double getX() {
-		return x;
-	}
-	public void setX(double x) {
-		this.x = x;
-	}
-	public double getY() {
-		return y;
-	}
-	public void setY(double y) {
 		this.y = y;
 	}
 	public void draw(GraphicsContext gc) {
@@ -38,6 +22,12 @@ public abstract class Position {
 	public boolean isShow() {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	public double getX() {
+		return x;
+	}
+	public double getY() {
+		return y;
 	}
 	
 }
